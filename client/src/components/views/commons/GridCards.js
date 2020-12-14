939c566a7fce532c810ab9ca604faa9c8d3bfceb
yusunjo <tnswh2023@gmail.com>
2020-12-14 22:05:36 +0900

    ActorToggle

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import {Col } from 'antd'
 
-function GridCards({movieId, movieName, image}) {
-    return (
-        <Col lg={6} md ={8} xs={24}>
-            <div style={{position: 'relative'}}>
-                <a href={`/movie/${movieId}`} >
-                    <img style={{width:'100%', height:'320px'}} src={image} alt={movieName}></img>
-                </a>
-            </div>
-        </Col>
-    )
+function GridCards({movieId, movieName, image , landingPage, characterName}) {
+    if(landingPage){
+        return (
+            <Col lg={6} md ={8} xs={24}>
+                <div style={{position: 'relative'}}>
+                    <a href={`/movie/${movieId}`} >
+                        <img style={{width:'100%', height:'320px'}} src={image} alt={movieName}></img>
+                    </a>
+                </div>
+            </Col>
+        )
+    }else{
+        return (
+            <Col lg={6} md ={8} xs={24}>
+                <div style={{position: 'relative'}}>
+                        <img style={{width:'100%', height:'320px'}} src={image} alt={characterName}></img>
+                </div>
+            </Col>
+        )
+    }
+    
 }
 
 export default GridCards
